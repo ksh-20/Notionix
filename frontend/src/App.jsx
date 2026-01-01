68121a39d8ecbb6451e15b72fab1cd9219b60520
Kshitij <kshitij24.srinivasan@gmail.com>
2026-01-01 19:20:07 +0530

    Frontend Setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import HomePage from './pages/HomePage'
+import CreatePage from './pages/CreatePage'
+import NoteDetailPage from './pages/NoteDetailPage'
+import { Route, Routes } from 'react-router'
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/create' element={<CreatePage />} />
+        <Route path='/note/:id' element={<NoteDetailPage />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App;
