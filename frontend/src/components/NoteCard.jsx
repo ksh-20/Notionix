4eb39f2df67daafc4425320479d20dc3eb1d3698
Kshitij <kshitij24.srinivasan@gmail.com>
2026-01-08 00:12:16 +0530

    Home Page

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -2,6 +2,8 @@ import { PenSquareIcon, Trash2Icon } from 'lucide-react'
 import React from 'react'
 import { Link } from 'react-router'
 
+import { formatDate } from '../lib/utils.js'
+
 const NoteCard = ({note}) => {
   return <Link to={`note/${note._id}`} className='card bg-base-100 hover:shadow-lg transition-all duration-200 
   border-t-4 border-solid border-[#00FF9D]'>
@@ -10,12 +12,12 @@ const NoteCard = ({note}) => {
         <p className='text-base-content/70 line-clamp-3'>{note.content}</p>
         <div className='card-actions justify-between items-center mt-4'>
             <span className='text-sm text-base-content/60'>
-                {note.createdAt}
+                {formatDate(new Date(note.createdAt))}
             </span>
             <div className='flex items-center gap-1'>
                 <PenSquareIcon className='size-4' />
-                <button>
-                    <Trash2Icon className='btn btn-ghost btn-xs text-error`'/>
+                <button className='btn btn-ghost btn-xs text-error'>
+                    <Trash2Icon className='size-4' />
                 </button>
             </div>
         </div>
@@ -23,4 +25,4 @@ const NoteCard = ({note}) => {
   </Link>
 }
 
-export default NoteCard;
+export default NoteCard
