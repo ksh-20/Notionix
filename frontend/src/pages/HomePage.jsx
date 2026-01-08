0a00db1b805bb47c9a2d0f368c6a105fe419085e
Kshitij <kshitij24.srinivasan@gmail.com>
2026-01-08 10:13:01 +0530

    Create and Delete Functionality

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,54 +1,63 @@
-import React, { useEffect, useState } from 'react'
-import Navbar from '../components/Navbar'
-import RateLimit from '../components/RateLimit'
-import NoteCard from '../components/NoteCard'
+import { useState } from "react";
+import { useEffect } from "react";
 
-import axios from 'axios'
-import toast from 'react-hot-toast'
+import toast from "react-hot-toast";
+// import axios from 'axios';
+import api from "../lib/axios";
+
+import Navbar from "../components/Navbar";
+import RateLimitedUI from "../components/RateLimit";
+import NoteCard from "../components/NoteCard";
+import NotesNotFound from "../components/NotesNotFound";
 
 const HomePage = () => {
-  const [isRateLimited, setRateLimited] = useState(false);
+  const [isRateLimited, setIsRateLimited] = useState(false);
   const [notes, setNotes] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchNotes = async() => {
+    const fetchNotes = async () => {
       try {
-        const res = await axios.get("http://localhost:5001/api/notes");
+        const res = await api.get("notes");
         console.log(res.data);
         setNotes(res.data);
-        setRateLimited(false);
+        setIsRateLimited(false);
       } catch (error) {
-        console.error("Error Fetching Notes", error);
-        if(error.response.status === 429) {
-          setRateLimited(true);
+        console.log("Error fetching notes");
+        console.log(error.response);
+        if (error.response?.status === 429) {
+          setIsRateLimited(true);
         } else {
-          toast.error("Failed to fetch Notes");
+          toast.error("Failed to load notes");
         }
       } finally {
         setLoading(false);
       }
-    }
-    fetchNotes();
-  },[])
-  return (
-    <div className='min-h-screen'>
-      <Navbar />
-      {isRateLimited && <RateLimit />}
+    };
 
-      <div className='max-w-7xl mx-auto p-4 mt-6'>
-        {loading && <div className='text-center text-primary py-10'>Loading Notes...</div>}
+    fetchNotes();
+  }, []);
+
+  return (
+    <div className="min-h-screen">
+      <Navbar />
+
+      {isRateLimited && <RateLimitedUI />}
+
+      <div className="max-w-7xl mx-auto p-4 mt-6">
+        {loading && <div className="text-center text-primary py-10">Loading notes...</div>}
+
+        {notes.length === 0 && !isRateLimited && <NotesNotFound />}
 
         {notes.length > 0 && !isRateLimited && (
-          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
-            {notes.map(note => {
-              <NoteCard key={note._id} note={note} />
-            })}
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {notes.map((note) => (
+              <NoteCard key={note._id} note={note} setNotes={setNotes} />
+            ))}
           </div>
         )}
       </div>
     </div>
-  )
-}
-
+  );
+};
 export default HomePage
